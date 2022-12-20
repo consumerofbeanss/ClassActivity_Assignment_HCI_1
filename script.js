ef48867ba381d513e0884a3782e2a4ff1ded3a4c
consumerofbeanss <114371990+consumerofbeanss@users.noreply.github.com>
2022-12-20 21:22:29 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//Please put your answer here
+$('#signup-modal').hide()
+$('.signup-show').click(function(){
+    $('#signup-modal').show()
+})
+$('#close-modal').click(function(){
+    $('#signup-modal').hide()
+})
