513167d246406aca1a0632122f8f0fb39173697f
consumerofbeanss <114371990+consumerofbeanss@users.noreply.github.com>
2022-12-20 21:23:09 +0700

    Delete ClassActivity_Assignment_HCI_1-main_Ostein directory

--- ClassActivity_Assignment_HCI_1-main_Ostein/script.js
+++
@@ -1,8 +0,0 @@
-//Please put your answer here
-$('#signup-modal').hide()
-$('.signup-show').click(function(){
-    $('#signup-modal').show()
-})
-$('#close-modal').click(function(){
-    $('#signup-modal').hide()
-})
